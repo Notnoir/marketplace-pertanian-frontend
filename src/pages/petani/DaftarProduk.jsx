eeb821bdc29462ddd9b677f9e32b7c4e925b0210
Notnoir <riza80448@gmail.com>
2025-06-17 01:29:52 +0700

    update alert

--- src/pages/petani/DaftarProduk.jsx
+++ src/pages/petani/DaftarProduk.jsx
@@ -247,7 +247,7 @@ export default function DaftarProduk() {
 
       {/* Delete Confirmation Modal */}
       {confirmDelete && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-lg max-w-md w-full shadow-xl">
             <div className="p-6">
               <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 bg-red-100 rounded-full">
